66c43991ac945ab3e1a6d82ab35778af594bd661
Gengbin Zheng <gengbin.zheng@intel.com>
2019-02-26 12:14:13 -0600

    fixup! coll: Implement ring algorithm for Allgather

--- src/mpi/coll/iallgather/iallgather_tsp_ring_algos.h
+++ src/mpi/coll/iallgather/iallgather_tsp_ring_algos.h
@@ -15,7 +15,19 @@
 
 #include "tsp_namespace_def.h"
 
-/* Routine to schedule a ring based allgather */
+/* Algorithm: schedule a ring based allgather
+ *
+ * In the first step, each process i sends its contribution to process
+ * i+1 and receives the contribution from process i-1 (with
+ * wrap-around).  From the second step onwards, each process i
+ * forwards to process i+1 the data it received from process i-1 in
+ * the previous step.  This takes a total of p-1 steps.
+ *
+ * Cost = (p-1).alpha + n.((p-1)/p).beta
+ *
+ * This algorithm is preferred to recursive doubling for long messages
+ * and small number of ranks.
+ */
 #undef FUNCNAME
 #define FUNCNAME MPIR_TSP_Iallgather_sched_intra_ring
 #undef FCNAME
@@ -26,11 +38,10 @@ int MPIR_TSP_Iallgather_sched_intra_ring(const void *sendbuf, int sendcount,
                                          MPIR_Comm * comm, MPIR_TSP_sched_t * sched)
 {
     int mpi_errno = MPI_SUCCESS;
-    int i, src, dst, copy_dst;
-    /* Temporary buffers to execute the ring algorithm */
-    void *buf1, *buf2, *data_buf, *rbuf, *sbuf;
-    int vtcs[3], send_id[3], recv_id[3], dtcopy_id[3];
-    int nvtcs;
+    int i, j, jnext, left, right;
+    int recv_id = -1;
+    int vtcs[2], r_vtcs[3];
+    int nvtcs = 0;
 
     int size = MPIR_Comm_size(comm);
     int rank = MPIR_Comm_rank(comm);
@@ -48,9 +59,7 @@ int MPIR_TSP_Iallgather_sched_intra_ring(const void *sendbuf, int sendcount,
     if (is_inplace) {
         sendcount = recvcount;
         sendtype = recvtype;
-        data_buf = (char *) recvbuf;
-    } else
-        data_buf = (char *) sendbuf;
+    }
 
     /* Get datatype info of sendtype and recvtype */
     MPIR_Datatype_get_extent_macro(sendtype, sendtype_extent);
@@ -61,89 +70,51 @@ int MPIR_TSP_Iallgather_sched_intra_ring(const void *sendbuf, int sendcount,
     MPIR_Type_get_true_extent_impl(recvtype, &recvtype_lb, &recvtype_true_extent);
     recvtype_extent = MPL_MAX(recvtype_extent, recvtype_true_extent);
 
-    /* Allocate space for temporary buffers */
-    buf1 = MPIR_TSP_sched_malloc(recvcount * recvtype_extent, sched);
-    buf2 = MPIR_TSP_sched_malloc(recvcount * recvtype_extent, sched);
-
-    if (is_inplace) {
-        /* Copy data to buf1 from sendbuf or recvbuf(in case of inplace) */
-        dtcopy_id[0] =
-            MPIR_TSP_sched_localcopy((char *) data_buf + rank * recvcount * recvtype_extent,
-                                     sendcount, sendtype, (char *) buf1, recvcount, recvtype, sched,
-                                     0, NULL);
-        MPL_DBG_MSG_FMT(MPIR_DBG_COLL, VERBOSE,
-                        (MPL_DBG_FDEST,
-                         "copying data  to tmp_buf  on add:%p on %d rank before for loop from sendbuf:%p and dtcopy_id:%d",
-                         buf1, rank, (char *) data_buf + rank * sendtype_extent, dtcopy_id[0]));
-    } else {
+    if (!is_inplace) {
         /* Copy your data into your recvbuf from your sendbuf */
-        MPIR_TSP_sched_localcopy((char *) sendbuf, sendcount, sendtype,
-                                 (char *) recvbuf + rank * recvcount * recvtype_extent,
-                                 recvcount, recvtype, sched, 0, NULL);
+        vtcs[0] = MPIR_TSP_sched_localcopy((char *) sendbuf, sendcount, sendtype,
+                                           (char *) recvbuf + rank * recvcount * recvtype_extent,
+                                           recvcount, recvtype, sched, 0, NULL);
 
-        /* Copy data from sendbuf to buf1 to send the data */
-        dtcopy_id[0] = MPIR_TSP_sched_localcopy((char *) data_buf, sendcount, sendtype,
-                                                (char *) buf1, recvcount, recvtype, sched, 0, NULL);
+        nvtcs = 1;
     }
 
     /* In ring algorithm src and dst are fixed */
-    src = (size + rank - 1) % size;
-    dst = (rank + 1) % size;
+    left = (size + rank - 1) % size;
+    right = (rank + 1) % size;
 
-    sbuf = buf1;
-    rbuf = buf2;
+    j = rank;
+    jnext = left;
 
     /* Ranks pass around the data (size - 1) times */
-    for (i = 0; i < size - 1; i++) {
-        /* Get new tag for each cycle so that the send-recv pairs are matched correctly */
+    /* irecv in reality has no dependence to anything, therefore potentially
+     * they can be issued all together when executing the scheduler, which
+     * may not be a good thing. Use r_vtcs to limit the number of posted recv
+     * to 3
+     */
+    for (i = 1; i < size; i++) {
+
         mpi_errno = MPIR_Sched_next_tag(comm, &tag);
         if (mpi_errno)
             MPIR_ERR_POP(mpi_errno);
 
-        if (i == 0) {
-            nvtcs = 1;
-            vtcs[0] = dtcopy_id[0];
-        } else {
-            nvtcs = 2;
-            vtcs[0] = recv_id[(i - 1) % 3];
-            vtcs[1] = send_id[(i - 1) % 3];
-        }
+        MPIR_TSP_sched_isend(((char *) recvbuf + j * recvcount * recvtype_extent),
+                             recvcount, recvtype, right, tag, comm, sched, nvtcs, vtcs);
 
-        send_id[i % 3] = MPIR_TSP_sched_isend((char *) sbuf, recvcount, recvtype,
-                                              dst, tag, comm, sched, nvtcs, vtcs);
+        recv_id = MPIR_TSP_sched_irecv(((char *) recvbuf + jnext * recvcount * recvtype_extent),
+                                       recvcount, recvtype, left, tag, comm, sched, i > 3 ? 1 : 0,
+                                       r_vtcs + i % 3);
+        r_vtcs[i % 3] = recv_id;
         MPL_DBG_MSG_FMT(MPIR_DBG_COLL, VERBOSE,
-                        (MPL_DBG_FDEST, "posting recv at address=%p, count=%d", rbuf,
+                        (MPL_DBG_FDEST, "posting recv at address=%p, count=%d",
+                         ((char *) recvbuf + jnext * recvcount * recvtype_extent),
                          size * recvcount));
 
-        if (i == 0)
-            nvtcs = 0;
-        else if (i == 1) {
-            nvtcs = 2;
-            vtcs[0] = send_id[(i - 1) % 3];
-            vtcs[1] = recv_id[(i - 1) % 3];
-        } else {
-            nvtcs = 3;
-            vtcs[0] = send_id[(i - 1) % 3];
-            vtcs[1] = dtcopy_id[(i - 2) % 3];
-            vtcs[2] = recv_id[(i - 1) % 3];
-        }
+        vtcs[0] = recv_id;
+        nvtcs = 1;
 
-        recv_id[i % 3] = MPIR_TSP_sched_irecv((char *) rbuf, recvcount, recvtype,
-                                              src, tag, comm, sched, nvtcs, vtcs);
-
-        copy_dst = (size + rank - i - 1) % size;        /* Destination offset of the copy */
-        dtcopy_id[i % 3] = MPIR_TSP_sched_localcopy((char *) rbuf, recvcount, recvtype,
-                                                    (char *) recvbuf +
-                                                    copy_dst * recvcount * recvtype_extent,
-                                                    recvcount, recvtype, sched, 1, &recv_id[i % 3]);
-        MPL_DBG_MSG_FMT(MPIR_DBG_COLL, VERBOSE,
-                        (MPL_DBG_FDEST, "copying from location=%p to location=%p",
-                         (char *) rbuf + rank * recvcount * recvtype_extent,
-                         (char *) recvbuf + copy_dst * recvcount * recvtype_extent));
-
-        data_buf = sbuf;
-        sbuf = rbuf;
-        rbuf = data_buf;
+        j = jnext;
+        jnext = (size + jnext - 1) % size;
     }
 
   fn_exit:
